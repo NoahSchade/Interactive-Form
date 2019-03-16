0c570084540b7547d681d896ca6764b3b3ea8ecb
Noah Schade <noah9@mail.com>
2019-03-16 13:57:26 -0400

    Add "Your Job Role" input text field when "Other" is selected from the drop down menu.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 $("#name").focus();
+
+const $other_title = $('#other-title');
+
+$other_title.hide();
+
+// $("select #title").change(change);
+
+document.getElementById("title").addEventListener("change", myFunction, true);
+
+function myFunction() {
+        if($("[value ='other']").is(':selected')){
+            $other_title.show();
+        } else {
+            $other_title.hide();
+        }
+}
+
+
+
+// function myFunction() {
+//     if($("[value='other']")) {
+//         $other_title.show();
+//     } else {
+//         $other_title.hide();
+//     }
+// }
+// });
