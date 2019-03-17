f6ce0e86ffd09db3e07166be71819ebf2aef03dc
Noah Schade <noah9@mail.com>
2019-03-17 10:46:26 -0400

    Update 'T-Shirt Info' section

--- js/script.js
+++ js/script.js
@@ -23,12 +23,14 @@ $("label[for ='color']").hide();
 $("select#color").hide();
 
 
+const jsPunsArr = [];
+const iArr = [];
+
 $("#design").change(function(){
     $("label[for ='color']").show();
     $("select#color").show();
     
     for(let i = 1; i <= $("#design option").length; i++){
-        // console.log($(`#design option:nth-child(${i})`).text());
 
         if($(`#design option:nth-child(${i})`).text() === "Select Theme"){
             $(`#design option:nth-child(${i})`).remove();
@@ -39,12 +41,15 @@ $("#design").change(function(){
             for(let i = 1; i <= $("#color option").length; i++){
                      const $colorText = $(`#color option:nth-child(${i})`).text();
                      const $colorElement = $(`#color option:nth-child(${i})`);
+                     const $colorValue = $(`#color option:nth-child(${i})`).val();
                      if($colorText.match(regex)){
+                        jsPunsArr.push($colorValue);
                         $colorElement.show();
                      } else {
                         $colorElement.hide();
                      }
              }
+             $("select#color").val(jsPunsArr)[0];
         }
 
         if($(`#design option:nth-child(${i})`).filter(':selected').text().match(/.*I.*/)) {
@@ -52,15 +57,21 @@ $("#design").change(function(){
             for(let i = 1; i <= $("#color option").length; i++){
                      const $colorText = $(`#color option:nth-child(${i})`).text();
                      const $colorElement = $(`#color option:nth-child(${i})`);
+                     const $colorValue = $(`#color option:nth-child(${i})`).val();
                      if($colorText.match(regex)){
+                        iArr.push($colorValue);
                         $colorElement.show();
                      } else {
                         $colorElement.hide();
                      }
              }
+             $("select#color").val(iArr)[0];
         }
     }
+    
+   
 });
+
 
 // var paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
 // var regex = /[A-Z]/g;
